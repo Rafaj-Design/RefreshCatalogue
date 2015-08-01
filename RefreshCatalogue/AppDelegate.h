9da6a6682a262aea75ae77da08ec31f6b9abbbde
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2015-08-01 06:34:10 +0100

    Initial Commit

---
+++ RefreshCatalogue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RefreshCatalogue
+//
+//  Created by Ondrej Rafaj on 01/08/2015.
+//  Copyright (c) 2015 Ridiculous Innovations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RefreshCatalogue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RefreshCatalogue
+//
+//  Created by Ondrej Rafaj on 01/08/2015.
+//  Copyright (c) 2015 Ridiculous Innovations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
